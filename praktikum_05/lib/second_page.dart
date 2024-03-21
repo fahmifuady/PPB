282531645f5c4e0acc137b2b5f3c2cafa5c1fe70
fahmifuady <husni.fuad22@gmail.com>
2024-03-21 21:20:31 +0700

    Practice_05: add variable and change constructor

--- praktikum_05/lib/second_page.dart
+++ praktikum_05/lib/second_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key}) : super(key: key);
+  final String? data;
+  const SecondPage({Key? key, this.data}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,11 +11,15 @@ class SecondPage extends StatelessWidget {
         title: const Text('Second Page'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('kembali'),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('kembali'),
+            ),
+          ],
         ),
       ),
     );

c28f8596293bde357e763dd0fb3a6df7d6d43150
Fahmi Fuady <husni.fuad22@gmail.com>
2024-06-08 22:09:14 +0700

    RestAPI: add method GET /mahasiswa/pembayaran

--- tutorial-api-Master/src/routes.php
+++ tutorial-api-Master/src/routes.php
@@ -14,4 +14,32 @@ return function (App $app) {
         // Render index view
         return $container->get('renderer')->render($response, 'index.phtml', $args);
     });
+
+
+    $app->get('/mahasiswa/pembayaran', function(Request $request, Response $response){
+        //disini dimasukan script sql
+        $sql = "SELECT mahasiswa.nim, nama,
+            CASE
+                WHEN jk='L' THEN 'Laki-Laki'
+                WHEN jk='P' THEN 'Perempuan'
+            END as jenis_kelamin , umur, ifnull(semester,0) as semester, ifnull(jumlah,0) as jumlah_spp
+            FROM mahasiswa LEFT JOIN spp_mahasiswa
+            ON mahasiswa.nim = spp_mahasiswa.nim WHERE mahasiswa.nim<>0";
+
+        $jk = $request->getQueryParam("jk");
+        $umur = $request->getQueryParam("umur");
+
+        if($jk<>null){
+            $sql = $sql." AND jk='$jk'";
+        };
+
+        if($umur<>null){
+            $sql = $sql." AND umur=$umur";
+        };
+        
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $response->withJson(["status"=>"success", "data"=>$result],200);
+    });          
 };

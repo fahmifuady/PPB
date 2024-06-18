1bbba0fde6cc545d3f4e615179680b0ae27ed0e3
fahmifuady <husni.fuad22@gmail.com>
2024-06-18 21:01:34 +0700

    chore: add cloud_firestore object

--- praktikum_08/lib/ui/home.dart
+++ praktikum_08/lib/ui/home.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:praktikum_06/ui/login.dart';
+
+final _firestore = FirebaseFirestore.instance;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
